9e5e71837150b22dbf2c373d23433a8479a11911
annamavroidi <151840814+annamavroidi@users.noreply.github.com>
2023-11-23 14:27:03 +0200

    Initial

---
+++ index.php
@@ -0,0 +1,41 @@
+<!-- saved from url=(0022)http://internet.e-mail -->
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Page Under Construction</title>
+<style type="text/css">
+<!--
+body {
+	background: url(35656722358.gif) center top no-repeat; margin:0; padding: 0; background-color: #F5F5F3;
+}
+.comments {
+	font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+	font-size: 60px;
+	font-weight: normal;
+	color: #ccc;
+	position:absolute;
+	width: 100%;
+ 	bottom:200px;
+	margin:0 auto; 
+
+}
+-->
+</style>
+</head>
+
+<body>
+
+<div class="comments">
+
+<?php
+$hostname = $_SERVER['SERVER_NAME']; 
+$hostname = str_replace('www.', '', $hostname); 
+?>
+
+
+  <center><? echo $hostname ?><br>
+  </center>
+</div>
+</body>
+</html>
